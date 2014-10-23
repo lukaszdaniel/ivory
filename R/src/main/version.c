d12fb4a12b56d3e95481951705813c2ec39061f7
lukasz.daniel@gmail.com <lukasz.daniel@gmail.com@c95f8428-fd29-ddb8-53fe-49f358986576>
2014-10-23 18:02:29 +0000

    Changes in welcoming screen + README-IVORY file.

--- R/src/main/version.c
+++ R/src/main/version.c
@@ -34,7 +34,7 @@ void attribute_hidden PrintGreeting(void)
     Rprintf("\n");
     PrintVersion_part_1(buf, 384);
     Rprintf("%s\n", buf);
-    Rprintf("This modified version of R is not part of the R project, so please do not report bugs\nvia r-bugs or the R website - instead refer to the author.\n\n");
+    Rprintf("Ivory is not part of the R project, so please do not report bugs\nvia r-bugs or the R website - instead refer to the author.\n\n");
     Rprintf(_("R is free software and comes with ABSOLUTELY NO WARRANTY.\n\
 You are welcome to redistribute it under certain conditions.\n\
 Type 'license()' or 'licence()' for distribution details.\n\n"));
@@ -142,7 +142,6 @@ void attribute_hidden PrintVersion_part_1(char *s, size_t len)
     SPRINTF_2("Platform: %s", R_PLATFORM);
     if(strlen(R_ARCH)) { SPRINTF_2("/%s", R_ARCH); }
     SPRINTF_2(" (%d-bit)\n", 8*(int)sizeof(void *));
-    strcat(s, "\nThis version of R supports extended localization\n");
-    strcat(s, "See README-LUKI file for details\n");
+    strcat(s, "\nIvory is a modified version of R. See README-IVORY file for details.\n");
     strcat(s, "Copyright (C) 2013-2014 Lukasz Daniel (lukasz.daniel@gmail.com)\n");
 }
