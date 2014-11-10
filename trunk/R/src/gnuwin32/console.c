b95927238c6ae8d928ca91cee5aed5a5e1033143
lukasz.daniel@gmail.com <lukasz.daniel@gmail.com@c95f8428-fd29-ddb8-53fe-49f358986576>
2014-11-10 15:57:02 +0000

    Updated to R-devel revision 66950.

--- trunk/R/src/gnuwin32/console.c
+++ trunk/R/src/gnuwin32/console.c
@@ -999,12 +999,9 @@ static void performCompletion(control c)
 	    consolewrites(c, CHAR(STRING_ELT(VECTOR_ELT(ans, POSSIBLE), i)));
 	}
 	if (alen > max_show) {
-	consolewrites(c, "\n");
-	    char *trunc = _("[... truncated]");
-	    const int bufsize = strlen(trunc);
-            char result [bufsize];
-            snprintf(result, bufsize, "%s", trunc);
-	    consolewrites(c, result);
+		consolewrites(c, "\n");
+	    const char *trunc = _("[... truncated]");
+	    consolewrites(c, trunc);
 	}
 	consolewrites(c, "\n");
 	p->wipe_completion = 1;
