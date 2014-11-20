9850300dfc08e34534f59437b30ebb7bfbbd1bb1
lukasz.daniel@gmail.com <lukasz.daniel@gmail.com@c95f8428-fd29-ddb8-53fe-49f358986576>
2014-11-20 13:26:08 +0000

    Changes in order to compile IVORY under windows

--- trunk/R/src/gnuwin32/bitmap/rbitmap.c
+++ trunk/R/src/gnuwin32/bitmap/rbitmap.c
@@ -36,6 +36,7 @@
  *
  */
 
+#include <localization.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <setjmp.h>
--- trunk/R/src/library/utils/src/localization.h
+++ trunk/R/src/library/utils/src/localization.h
@@ -5,17 +5,17 @@
 
 #ifdef ENABLE_NLS
  #include <libintl.h>
-  #ifdef Win32
-   #define _(String) libintl_dgettext ("utils", String)
-   #undef gettext /* needed for graphapp */
-  #else
-   #define _(String) dgettext ("utils", String)
-  #endif
+ #define _(String) libintl_dgettext ("utils", String)
+ #undef gettext /* needed for graphapp */
  #define gettext_noop(String) String
  #define N_(String) gettext_noop (String)
+ #define G_(String) libintl_dgettext("RGui", String)
+ #define GN_(String) gettext_noop (String)
 #else /* not NLS */
  #define _(String) (String)
  #define N_(String) String
+ #define G_(String) (String)
+ #define GN_(String) String
  #define ngettext(String, StringP, N) (N > 1 ? StringP: String)
  #define dngettext(Domain, String, StringP, N) ngettext(String, StringP, N)
 #endif
