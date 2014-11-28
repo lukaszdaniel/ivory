cb84b637ef8c150c5f1d9feb312e4e3d02e307d7
lukasz.daniel@gmail.com <lukasz.daniel@gmail.com@c95f8428-fd29-ddb8-53fe-49f358986576>
2014-11-28 18:20:25 +0000

    Updated to R-devel revision 67072.

--- R/src/main/util.c
+++ R/src/main/util.c
@@ -1858,15 +1858,14 @@ const char* ucol_getLocaleByType(const UCollator *coll,
 #endif
 
 static UCollator *collator = NULL;
-
-static Rboolean collationLocaleSet = FALSE;
+static int collationLocaleSet = 0;
 
 /* called from platform.c */
 void attribute_hidden resetICUcollator(void)
 {
     if (collator) ucol_close(collator);
     collator = NULL;
-    collationLocaleSet = FALSE;
+    collationLocaleSet = 0;
 }
 
 static const struct {
@@ -1945,19 +1944,23 @@ SEXP attribute_hidden do_ICUset(SEXP call, SEXP op, SEXP args, SEXP rho)
 		ucol_close(collator);
 		collator = NULL;
 	    }
-	    if(strcmp(s, "none")) {
-		if(streql(s, "default")) 
-		    uloc_setDefault(getLocale(), &status);
-		else uloc_setDefault(s, &status);
-		if(U_FAILURE(status))
-		    error(_("failed to set ICU locale %s (%d)"), s, status);
-		collator = ucol_open(NULL, &status);
-		if (U_FAILURE(status)) {
-		    collator = NULL;
-		    error(_("failed to open ICU collator (%d)"), status);
+	    if(streql(s, "ASCII")) {
+		collationLocaleSet = 2;
+	    } else {
+		if(strcmp(s, "none")) {
+		    if(streql(s, "default")) 
+			uloc_setDefault(getLocale(), &status);
+		    else uloc_setDefault(s, &status);
+		    if(U_FAILURE(status))
+			error(_("failed to set ICU locale %s (%d)"), s, status);
+		    collator = ucol_open(NULL, &status);
+		    if (U_FAILURE(status)) {
+			collator = NULL;
+			error(_("failed to open ICU collator (%d)"), status);
+		    }
 		}
+		collationLocaleSet = 1;
 	    }
-	    collationLocaleSet = TRUE;
 	} else {
 	    int i, at = -1, val = -1;
 	    for (i = 0; ATtable[i].str; i++)
@@ -1988,7 +1991,9 @@ SEXP attribute_hidden do_ICUget(SEXP call, SEXP op, SEXP args, SEXP rho)
     const char *ans = "unknown", *res;
     checkArity(op, args);
 
-    if(collator) {
+    if (collationLocaleSet == 2) {
+        ans = "ASCII";
+    } else if(collator) {
 	UErrorCode  status = U_ZERO_ERROR;
 	int type = asInteger(CAR(args));
 	if (type < 1 || type > 2)
@@ -2008,7 +2013,7 @@ attribute_hidden
 int Scollate(SEXP a, SEXP b)
 {
     if (!collationLocaleSet) {
-	collationLocaleSet = TRUE;
+	collationLocaleSet = 1;
 #ifndef Win32
 	if (strcmp("C", getLocale()) ) {
 #else
@@ -2027,7 +2032,9 @@ int Scollate(SEXP a, SEXP b)
 	}
     }
     if (collator == NULL)
-	return strcoll(translateChar(a), translateChar(b));
+	return collationLocaleSet == 2 ?
+	    strcmp(translateChar(a), translateChar(b)) :
+	    strcoll(translateChar(a), translateChar(b));
 
     UCharIterator aIter, bIter;
     const char *as = translateCharUTF8(a), *bs = translateCharUTF8(b);
