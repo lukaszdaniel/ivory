943f937f73554987ec9a9855926122861acdf11a
lukasz.daniel@gmail.com <lukasz.daniel@gmail.com@c95f8428-fd29-ddb8-53fe-49f358986576>
2014-12-04 11:15:22 +0000

    Updated to R-devel revision 67101.

--- trunk/R/src/main/objects.c
+++ trunk/R/src/main/objects.c
@@ -1564,12 +1564,13 @@ SEXP R_do_new_object(SEXP class_def)
 	error(_("trying to generate an object from a virtual class (\"%s\")"), translateChar(asChar(e)));
     }
     e = R_do_slot(class_def, s_className);
-    value = duplicate(R_do_slot(class_def, s_prototype));
+    PROTECT(value = duplicate(R_do_slot(class_def, s_prototype)));
     if(TYPEOF(value) == S4SXP || getAttrib(e, R_PackageSymbol) != R_NilValue)
     { /* Anything but an object from a base "class" (numeric, matrix,..) */
 	setAttrib(value, R_ClassSymbol, e);
 	SET_S4_OBJECT(value);
     }
+    UNPROTECT(1); /* value */
     vmaxset(vmax);
     return value;
 }
@@ -1624,8 +1625,11 @@ SEXP asS4(SEXP s, Rboolean flag, int complete)
     if(flag == IS_S4_OBJECT(s))
 	return s;
     PROTECT(s);
-    if(MAYBE_SHARED(s))
+    if(MAYBE_SHARED(s)) {
 	s = shallow_duplicate(s);
+	UNPROTECT(1);
+	PROTECT(s);
+    }
     if(flag) SET_S4_OBJECT(s);
     else {
 	if(complete) {
