d2c084456082287661709d9babfbfb24fa7b3313
lukasz.daniel@gmail.com <lukasz.daniel@gmail.com@c95f8428-fd29-ddb8-53fe-49f358986576>
2014-12-08 11:24:36 +0000

    'win-nls.h' header is a dummy file now.

--- trunk/R/src/gnuwin32/win-nls.h
+++ trunk/R/src/gnuwin32/win-nls.h
@@ -18,20 +18,9 @@
  *  http://www.r-project.org/Licenses/
  */
 
-#ifdef ENABLE_NLS
- #include <libintl.h>
- #define _(String) dgettext("R", String)
- #define n_(String, StringP, N) dngettext("R", String, StringP, N)
- #define gettext_noop(String) String
- #define N_(String) gettext_noop (String)
- #define G_(String) dgettext("RGui", String)
- #define GN_(String) gettext_noop (String)
-#else /* not NLS */
- #define _(String) (String)
- #define N_(String) String
- #define G_(String) (String)
- #define GN_(String) String
- #define ngettext(String, StringP, N) (N > 1 ? StringP: String)
- #define dngettext(Domain, String, StringP, N) ngettext(String, StringP, N)
- #define n_(String, StringP, N) ngettext(String, StringP, N)
-#endif
+/*
+ * IVORY:
+ * Definitions have been merged in 'Localization.h' header.
+ * Please use 'Localization.h' instead of 'win-nls.h'.
+ */
+#include <Localization.h>
