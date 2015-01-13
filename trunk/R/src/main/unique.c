59c8c11e559e06b795e516a37b08990c9601b5ae
lukasz.daniel@gmail.com <lukasz.daniel@gmail.com@c95f8428-fd29-ddb8-53fe-49f358986576>
2015-01-13 06:47:45 +0000

    Updated to R-devel revision 67443.

--- trunk/R/src/main/unique.c
+++ trunk/R/src/main/unique.c
@@ -1236,6 +1236,9 @@ static SEXP subDots(SEXP rho)
     if (dots == R_MissingArg)
 	return dots;
 
+    if (!isPairList(dots))
+        error(_("'...' is not a pairlist"));
+    
     len = length(dots);
     PROTECT(rval=allocList(len));
     for(a = dots, b = rval, i = 1; i <= len; a = CDR(a), b = CDR(b), i++) {
@@ -1257,7 +1260,7 @@ SEXP attribute_hidden do_matchcall(SEXP call, SEXP op, SEXP args, SEXP env)
 {
     SEXP formals, actuals, rlist;
     SEXP funcall, f, b, rval, sysp, t1, t2, tail;
-    RCNTXT *cptr;
+//    RCNTXT *cptr;
     int expdots;
 
     checkArity(op,args);
@@ -1269,67 +1272,14 @@ SEXP attribute_hidden do_matchcall(SEXP call, SEXP op, SEXP args, SEXP env)
 
     if (TYPEOF(funcall) != LANGSXP)
 	error(_("invalid '%s' argument"), "call");
+    
+    b = CAR(args);
+    if (TYPEOF(b) != CLOSXP)
+        error(_("invalid '%s' argument"), "definition");
 
-    /* Get the function definition */
-    sysp = R_GlobalContext->sysparent;
-
-    if (TYPEOF(CAR(args)) == NILSXP) {
-	/* Get the env that the function containing */
-	/* matchcall was called from. */
-	cptr = R_GlobalContext;
-	while (cptr != NULL) {
-	    if (cptr->callflag & CTXT_FUNCTION && cptr->cloenv == sysp)
-		break;
-	    cptr = cptr->nextcontext;
-	}
-	if ( cptr == NULL ) {
-	    sysp = R_GlobalEnv;
-	    errorcall(R_NilValue,
-		      _("'match.call()' function was called from outside a function"));
-	} else
-	    sysp = cptr->sysparent;
-	if (cptr != NULL)
-	    /* Changed to use the function from which match.call was
-	       called as recorded in the context.  This change is
-	       needed in case the current function is computed in a
-	       way that cannot be reproduced by a second computation,
-	       or if it is a registered S3 method that is not
-	       lexically visible at the call site.
-
-	       There is one particular case where this represents a
-	       change from previous semantics: The definition is NULL,
-	       the call is supplied explicitly, and the function in
-	       the call is NOT the current function.  The new behavior
-	       is to ignore the function in the call and use the
-	       current function.  This is consistent with (my reading
-	       of) the documentation in both R and Splus.  However,
-	       the old behavior of R was consistent with the behavior
-	       of Splus (and inconsistent with the documentation in
-	       both cases).
-
-	       The previous semantics for this case can be restored by
-	       having the .Internal receive an additional argument
-	       that indicates whether the call was supplied explicitly
-	       or missing, and using the function recorded in the
-	       context only if the call was not supplied explicitly.
-	       The documentation should also be changed to be
-	       consistent with this behavior.  LT */
-	    PROTECT(b = duplicate(cptr->callfun));
-	else if ( TYPEOF(CAR(funcall)) == SYMSXP )
-	    PROTECT(b = findFun(CAR(funcall), sysp));
-	else
-	    PROTECT(b = eval(CAR(funcall), sysp));
-
-	if (TYPEOF(b) != CLOSXP)
-	    error(_("unable to find a closure from within which 'match.call()' function was called"));
-
-    }
-    else {
-	/* It must be a closure! */
-	PROTECT(b = CAR(args));
-	if (TYPEOF(b) != CLOSXP)
-	    error(_("invalid '%s' argument"), "definition");
-    }
+    sysp = CAR(CDDDR(args));
+    if (!isEnvironment(sysp))
+        error(_("'%s' argument must be an environment"), "envir");
 
     /* Do we expand ... ? */
 
@@ -1356,7 +1306,7 @@ SEXP attribute_hidden do_matchcall(SEXP call, SEXP op, SEXP args, SEXP env)
     /* now to splice t2 into the correct spot in actuals */
     if (t2 != R_MissingArg ) {	/* so we did something above */
 	if( CAR(actuals) == R_DotsSymbol ) {
-	    UNPROTECT(1);
+            UNPROTECT(1);
 	    actuals = listAppend(t2, CDR(actuals));
 	    PROTECT(actuals);
 	}
@@ -1372,7 +1322,7 @@ SEXP attribute_hidden do_matchcall(SEXP call, SEXP op, SEXP args, SEXP env)
 	}
     } else { /* get rid of it */
 	if( CAR(actuals) == R_DotsSymbol ) {
-	    UNPROTECT(1);
+            UNPROTECT(1);
 	    actuals = CDR(actuals);
 	    PROTECT(actuals);
 	}
@@ -1407,7 +1357,7 @@ SEXP attribute_hidden do_matchcall(SEXP call, SEXP op, SEXP args, SEXP env)
     PROTECT(rval = allocSExp(LANGSXP));
     SETCAR(rval, duplicate(CAR(funcall)));
     SETCDR(rval, rlist);
-    UNPROTECT(4);
+    UNPROTECT(3);
     return rval;
 }
 
