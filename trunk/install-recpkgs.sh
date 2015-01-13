233fbd1a0b666e6b67244ff94531c03053abf656
lukasz.daniel@gmail.com <lukasz.daniel@gmail.com@c95f8428-fd29-ddb8-53fe-49f358986576>
2015-01-13 18:34:00 +0000

    Useful ivory's tools.

---
+++ trunk/install-recpkgs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+source conf-names.sh
+
+
+
+source svn-cleanup.sh $RECIVORY
+cd $RECIVORY
+svn status --no-ignore | egrep '^[?I]' | cut -c9- | xargs -d \\n rm -r &> /dev/null
+
+if [ "$1" != "" ]; then
+ RECPKGS=$1
+fi;
+
+ for pkg in $RECPKGS; do
+  cd $pkg
+  rm MD5
+  md5deep -r -l * > MD5
+  cd ..
+  package=`echo $pkg | sed -e 's/\///g'`;
+  targz=`find ../$IVORY/src/library/Recommended/$package*.tar.gz`
+  tar=`echo $targz | sed -e 's/\.gz//'`
+  echo "Updating" $targz
+  gunzip -f $targz
+  tar --delete -f $tar $pkg
+  tar rf $tar $pkg
+  gzip $tar
+ done
+ source $IVORY/tools/link-recommended
+
+
