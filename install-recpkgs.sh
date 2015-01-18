5c6b200ca721329fd159975e5d1e4724bf2677a6
lukasz.daniel@gmail.com <lukasz.daniel@gmail.com@c95f8428-fd29-ddb8-53fe-49f358986576>
2015-01-18 11:43:59 +0000

    'install-recpkgs.sh' can now replace old version of recommended package with the new one.

--- install-recpkgs.sh
+++ install-recpkgs.sh
@@ -8,25 +8,28 @@ source svn-cleanup.sh $RECIVORY
 cd $RECIVORY
 svn status --no-ignore | egrep '^[?I]' | cut -c9- | xargs -d \\n rm -r &> /dev/null
 
-if [ "$1" != "" ]; then
+if test -n "$1"; then
  RECPKGS=$1
 fi;
 
  for pkg in $RECPKGS; do
   cd $pkg
+  version=`sed -n -e '/Version:/p' DESCRIPTION | cut -d" " -f2`
   rm MD5
   md5deep -r -l * > MD5
   cd ..
-  package=`echo $pkg | sed -e 's/\///g'`;
-  targz=`find ../$IVORY/src/library/Recommended/$package*.tar.gz`
-  tar=`echo $targz | sed -e 's/\.gz//'`
-  echo "Updating" $targz
-  gunzip -f $targz
-  tar --delete -f $tar $pkg
-  tar rf $tar $pkg
-  gzip $tar
+  if test -f ../${IVORY}/src/library/Recommended/${pkg}_${version}.tar.gz; then
+   echo "Updating package '"${pkg}"' version" ${version}
+   tar -czf ../${IVORY}/src/library/Recommended/${pkg}_${version}.tar.gz ${pkg}
+  else
+   echo "Adding package '"${pkg}"' version" ${version}
+   svn delete ../${IVORY}/src/library/Recommended/${pkg}_*.tar.gz
+   tar -czf ../${IVORY}/src/library/Recommended/${pkg}_${version}.tar.gz ${pkg}
+   svn add ../${IVORY}/src/library/Recommended/${pkg}_${version}.tar.gz
+  fi;
  done
-cd ..
- source $IVORY/tools/link-recommended
 
-
+ cd ../${IVORY}/src/library/Recommended
+  for i in ${RECPKGS} ; do
+    ln -sf $i*.tar.gz ${i}.tgz
+  done
