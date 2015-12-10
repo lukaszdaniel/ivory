8fd5885bc51cb8cd850ab7e9e7e60c4a2b28aa8c
Lukasz Daniel <lukasz.daniel@gmail.com>
2015-12-10 19:43:56 +0100

    Updated to R-devel revision 69757.

--- R/src/library/methods/src/methods_list_dispatch.c
+++ R/src/library/methods/src/methods_list_dispatch.c
@@ -1043,8 +1043,6 @@ SEXP R_dispatchGeneric(SEXP fname, SEXP ev, SEXP fdef)
     /* the rest of this is identical to R_standardGeneric;
        hence the f=method to remind us  */
     f = method;
-    if(isObject(f))
-	f = R_loadMethod(f, fname, ev);
     switch(TYPEOF(f)) {
     case CLOSXP:
     {
@@ -1052,6 +1050,8 @@ SEXP R_dispatchGeneric(SEXP fname, SEXP ev, SEXP fdef)
             val = R_deferred_default_method();
         } else {
             SEXP R_execMethod(SEXP, SEXP);
+            if(isObject(f))
+                f = R_loadMethod(f, fname, ev);
             PROTECT(f); nprotect++; /* is this needed?? */
             val = R_execMethod(f, ev);
         }
