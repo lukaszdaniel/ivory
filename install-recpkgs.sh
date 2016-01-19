3b7a8fb3b1ace52ba283d58cc5ca3bfac4bd56f8
Lukasz Daniel <lukasz.daniel@gmail.com>
2016-01-19 22:42:07 +0100

    Small fix for creating MD5 file.

--- install-recpkgs.sh
+++ install-recpkgs.sh
@@ -17,7 +17,8 @@ fi;
   version=`sed -n -e '/Version:/p' DESCRIPTION | cut -d" " -f2`
   rm MD5
   #md5deep -r -l * > MD5
-  find -type f -print0 | xargs -0 md5sum | sed -e 's/ .\//*/' > MD5
+  find -type f -print0 | xargs -0 md5sum | sed -e 's/ .\//*/' > ../MD5
+  mv ../MD5 .
   cd ..
   if test -f ../${IVORY}/src/library/Recommended/${pkg}_${version}.tar.gz; then
    echo "Updating package '"${pkg}"' version" ${version}
