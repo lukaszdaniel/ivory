3f112c131c2c3f9216a42072501144087859e685
Lukasz Daniel <lukasz.daniel@gmail.com>
2016-02-03 22:57:10 +0100

    Updated to R-devel revision 70081.

--- R/src/library/grDevices/src/cairo/cairoBM.c
+++ R/src/library/grDevices/src/cairo/cairoBM.c
@@ -434,6 +434,7 @@ BMDeviceDriver(pDevDesc dd, int kind, const char *filename,
     dd->startfill = xd->fill;
     dd->startlty = LTY_SOLID;
     dd->startfont = 1;
+    dd->startgamma = 1;
     dd->displayListOn = FALSE;
     dd->deviceSpecific = (void *) xd;
 
