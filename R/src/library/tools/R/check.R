bb9abaa140c01dee06ac6a98f05a6f390eec2c2f
Lukasz Daniel <lukasz.daniel@gmail.com>
2020-01-17 20:51:02 +0100

    Updated to R-devel revision 77670.

--- R/src/library/tools/R/check.R
+++ R/src/library/tools/R/check.R
@@ -5161,22 +5161,23 @@ add_dummies <- function(dir, Log)
                          "many_depends",
                          "skipped",
                          "hdOnly",
-                         "orphaned2",
-                         "orphaned", ## change later
+                         "orphaned2", "orphaned",
                          if(!check_incoming) "bad_engine")
             if(!all(names(res) %in% allowed)) {
                 errorLog(Log)
                 printLog(Log, paste(out, collapse = "\n"), "\n")
                 if(length(res$suggested_but_not_installed))
                    wrapLog(gettext("The suggested packages are required for a complete check.\nChecking can be attempted without them by setting the environment variable _R_CHECK_FORCE_SUGGESTS_ to a false value.\n\n", domain = "R-tools"))
-                if(length(res$orphaned2))
-                    wrapLog(gettext("Suggested packages should be used conditionally: this is particularly important for orphaned ones\n\n", domain = "R-tools"))
                 wrapLog(msg_DESCRIPTION)
                 summaryLog(Log)
                 do_exit(1L)
             } else {
-                noteLog(Log)
+                if(length(res[["orphaned"]])) warningLog(Log) else noteLog(Log)
                 printLog(Log, paste(out, collapse = "\n"))
+                ## if(length(res$orphaned2))
+                ##     wrapLog("\nSuggested packages need to be used conditionally:",
+                ##             "this is particularly important for",
+                ##             "orphaned ones.\n")
             }
         } else resultLog(Log, gettext("OK", domain = "R-tools"))
     }
@@ -5729,7 +5730,7 @@ add_dummies <- function(dir, Log)
         Sys.setenv("_R_CHECK_DATALIST_" = "TRUE")
         if(!WINDOWS) Sys.setenv("_R_CHECK_BASHISMS_" = "TRUE")
         Sys.setenv("_R_CLASS_MATRIX_ARRAY_" = "TRUE")
-        ## Sys.setenv("_R_CHECK_ORPHANED_" = "TRUE")
+        Sys.setenv("_R_CHECK_ORPHANED_" = "TRUE")
         R_check_vc_dirs <- TRUE
         R_check_executables_exclusions <- FALSE
         R_check_doc_sizes2 <- TRUE
